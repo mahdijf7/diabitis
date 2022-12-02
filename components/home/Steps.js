0d8f6a237bc16113a7c49c7d4cd042fc3c9ec5c5
sahar bolboliankhah <sahar.b@t1intelligence.com>
2022-12-02 22:30:09 +0330

    part 4

--- components/home/Steps.js
+++ components/home/Steps.js
@@ -19,11 +19,45 @@ const Steps = () => {
     boxShadow: theme.customShadows.widget,
     borderRadius: "20px",
     border: theme.customShadows.border,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    p: { lg: "5%", md: "3%" },
+  };
+  const list = [
+    {
+      img: "/images/home/cv.svg",
+      text: t.home.cv,
+    },
+    {
+      img: "/images/home/pay.svg",
+      text: t.home.pay,
+    },
+    {
+      img: "/images/home/account.svg",
+      text: t.home.account,
+    },
+  ];
+  const Card = ({ index }) => {
+    return (
+      <Box textAlign="center">
+        <Image
+          src={list[index].img}
+          width={isMatch ? "90px" : "100px"}
+          height={isMatch ? "90px" : "100px"}
+        />
+        <Typography
+          color="text.hint"
+          component="h2"
+          sx={{ typography: { md: "body1", lg: "h5" } }}
+        >
+          {list[index].text}
+        </Typography>
+      </Box>
+    );
   };
   return (
     <Container disableGutters maxWidth={false}>
-      <Divider sx={{ borderStyle: "dashed" }}>tetss</Divider>
-
       <Typography
         color="text.primary"
         sx={{ typography: { md: "h4", lg: "h2" } }}
@@ -34,18 +68,17 @@ const Steps = () => {
         {t.home.stepsTitle}
       </Typography>
       <Box width="100%" height="300px" display="flex">
-        <Box width={{ lg: isMatch ? "15%" : "calc(50vw - 500px)", md: "10%" }}>
-          <Box height="20%" className={style.dash}></Box>
-          {/* <Box height="70%"></Box> */}
-        </Box>
+        <Box
+          height="20%"
+          className={style.dash}
+          width={{ lg: isMatch ? "15%" : "calc(49vw - 500px)", md: "10%" }}
+        />
         <Box display="flex" width={{ lg: "70%", md: "80%" }} maxWidth="1000px">
-          <Box width="27%">
-            <Box height={{ lg: "80%", md: "70%" }} sx={styleCard}></Box>
-            {/* <Box height="30%" bgcolor="yellow"></Box> */}
+          <Box width="27%" height={{ lg: "80%", md: "70%" }} sx={styleCard}>
+            <Card index={0} />
           </Box>
           <Box display="flex" width="9.5%">
-            <Box width="40%" height="50%" className={style.dash}></Box>
-
+            <Box width="40%" height="50%" className={style.dash} />
             <Box width="20%" display="flex" alignItems="center">
               <Image
                 src={"/images/home/arrow.svg"}
@@ -54,27 +87,35 @@ const Steps = () => {
                 style={{ marginTop: "30%" }}
               />
             </Box>
-            <Box width="40%" height="50%" className={style.dash}></Box>
-            {/* <Box height="40%"></Box> */}
+            <Box width="40%" height="50%" className={style.dash} />
           </Box>
           <Box width="27%">
-            <Box height="20%"></Box>
-            <Box height={{ lg: "80%", md: "70%" }} sx={styleCard}></Box>
+            <Box height="20%" />
+            <Box height={{ lg: "80%", md: "70%" }} sx={styleCard}>
+              <Card index={1} />
+            </Box>
           </Box>
-          <Box width="9.5%">
-            <Box height="50%"></Box>
-            <Divider>A</Divider>
-            <Box height="40%"></Box>
+          <Box display="flex" width="9.5%">
+            <Box width="40%" height="50%" className={style.dash} />
+            <Box width="20%" display="flex" alignItems="center">
+              <Image
+                src={"/images/home/arrow.svg"}
+                width="20px"
+                height="20px"
+                style={{ marginTop: "30%" }}
+              />
+            </Box>
+            <Box width="40%" height="50%" className={style.dash} />
           </Box>
-          <Box width="27%">
-            <Box height={{ lg: "80%", md: "70%" }} sx={styleCard}></Box>
-            {/* <Box height="30%" bgcolor="yellow"></Box> */}
+          <Box width="27%" height={{ lg: "80%", md: "70%" }} sx={styleCard}>
+            <Card index={2} />
           </Box>
         </Box>
-        <Box width={{ lg: isMatch ? "15%" : "calc(50vw - 500px)", md: "10%" }}>
-          <Box height="20%" className={style.dash}></Box>
-          {/* <Box height="70%"></Box> */}
-        </Box>
+        <Box
+          height="20%"
+          className={style.dash}
+          width={{ lg: isMatch ? "15%" : "calc(50vw - 500px)", md: "10%" }}
+        />
       </Box>
     </Container>
   );
--- components/layouts/footer/index.js
+++ components/layouts/footer/index.js
@@ -1,5 +1,115 @@
+import { Box, Container, Typography } from "@mui/material";
+import Image from "next/image";
+import t from "../../../public/locales/defualt/common.json";
+import logo from "../../../public/images/logo.svg";
+
 const Footer = () => {
-  return <></>;
+  const list = [
+    {
+      text: t.footer.subTitle1,
+    },
+    {
+      text: t.footer.subTitle2,
+    },
+    {
+      text: t.footer.subTitle3,
+    },
+    {
+      text: t.footer.subTitle4,
+    },
+  ];
+  const contact = [
+    {
+      text: t.footer.location,
+      icon: "/images/footer/location.svg",
+    },
+    {
+      text: t.footer.call,
+      icon: "/images/footer/phone.svg",
+    },
+    {
+      text: t.footer.email,
+      icon: "/images/footer/email.svg",
+    },
+  ];
+  const icons = [
+    "/images/footer/facebook.svg",
+    "/images/footer/twitter.svg",
+    "/images/footer/linkedin.svg",
+    "/images/footer/instagram.svg",
+  ];
+  return (
+    <Container
+      disableGutters
+      maxWidth={false}
+      sx={{
+        backgroundColor: "secondary.main",
+        p: "25px 60px 50px 60px",
+        display: "flex",
+        mt: { lg: "60px", md: "45px", xs: "30px" },
+      }}
+    >
+      <Box width="42.6%">
+        <Image src={"/images/logo.svg"} width="53px" height="55px" />
+        <Typography
+          color="common.white"
+          sx={{ typography: { md: "h4", lg: "h2" }, pb: "20px", pt: "5px" }}
+          component="h1"
+        >
+          {t.footer.title}
+        </Typography>
+        {list.map((el, index) => (
+          <Typography
+            key={index}
+            color="common.white"
+            sx={{ typography: { md: "subtitle5", lg: "subtitle4" }, pb: "8px" }}
+            component="h2"
+          >
+            {el.text}
+          </Typography>
+        ))}
+      </Box>
+      <Box width="30%" display="flex" alignItems="flex-end">
+        {icons.map((el, index) => (
+          <Box key={index} mx={{lg:"12px",md:"7px"}}>
+            <Image src={el} width="27px" height="27px" />
+          </Box>
+        ))}
+      </Box>
+      <Box width="25%">
+        <Typography
+          color="common.white"
+          sx={{
+            typography: { md: "subtitle3", lg: "body2" },
+            pb: "20px",
+            pt: "80px",
+            px: "15%",
+          }}
+          component="h1"
+        >
+          {t.footer.contact}
+        </Typography>
+        {contact.map((el, index) => (
+          <Box display="flex" key={index} alignItems="flex-start" mb="8px">
+            <Box width="15%">
+              <Image src={el.icon} width="20px" height="20px" />
+            </Box>
+            <Box width="85%">
+              <Typography
+                color="common.white"
+                sx={{
+                  typography: { md: "subtitle5", lg: "subtitle4" },
+                }}
+                component="h2"
+              >
+                {el.text}
+              </Typography>
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </Container>
+  );
 };
 
 export default Footer;
