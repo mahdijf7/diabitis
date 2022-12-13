4e03276cabcbd06c7d942bbed07a41ccda81967e
sahar bolboliankhah <sahar.b@t1intelligence.com>
2022-12-13 12:26:42 +0330

    fix

--- pages/news.js
+++ pages/news.js
@@ -2,7 +2,8 @@
 import Content from "../components/news";
 
 export default function Index() {
-  return <Content />;
+  // return <Content />;
+  return <></>;
 }
 
 export async function getStaticProps() {
